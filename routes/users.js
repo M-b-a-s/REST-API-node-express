076bd9f9e4a7d4fa211354daeeb281d2039bdea9
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2023-04-11 15:23:15 +0100

    view specific user

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,31 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "Ernest",
-    lastName: "Mbas",
-    age: 25,
-  },
-];
+let users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
 });
 
 router.post('/', (req, res) => {
-
     const user = req.body;
-
-    users.push(user);
+    
+    users.push({ ...user, id: uuidv4() });
 
     res.send(`User with the name ${user.firstName} added to the database`);
-})
+});
+
+
+router.get('/users:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.i === id)
+
+  res.send(foundUser)
+});
+
+router.delete('/users:id')
 
 export default router;
